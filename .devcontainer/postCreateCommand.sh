1c4f7a57c959c364c85f6f268fb7bf35d033d0e8
Ludovic Ortega <ludovic.ortega@adminafk.fr>
2025-06-23 00:19:27 +0200

    feat: update template dependencies
    Signed-off-by: Ludovic Ortega <ludovic.ortega@adminafk.fr>

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
 uv sync --all-extras
+
+# nixos fix
+# see https://github.com/microsoft/vscode-remote-release/issues/11024
+git config --global gpg.program "/usr/bin/gpg"
+
 pulumi login file://~/pulumi-data
 pulumi stack select dev
